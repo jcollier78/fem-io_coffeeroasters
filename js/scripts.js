e90979cc7197ab666e50104b93ecb8c0f2d0db15
= <=>
2021-08-24 14:18:43 +0800

    modal working in mobile version

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,22 @@ menuIcon.addEventListener('click', () => {
     }
     
 });
+
+
+// modal script
+const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+
+function toggleModal(){
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event){
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
