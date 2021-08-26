321f1a92f1d7d2e53807e98e05ee5a0d7bf6538f
= <=>
2021-08-26 16:03:46 +0800

    js order summary mostly working

--- js/scripts.js
+++ js/scripts.js
@@ -61,3 +61,80 @@ trigger.addEventListener("click", toggleModal);
 closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
+
+// order script
+const options = document.querySelectorAll("input[type=radio]");
+let html;
+let htmlModal;
+
+// add click event to each radio input to 
+options.forEach(elem => {
+    elem.addEventListener('click', (event)=>{
+        
+        //check if coffee type is capsule and disable grind options acoordion
+        if (document.getElementById('capsule').checked) {
+            if (document.getElementById('grind-option').classList.contains("open")) {
+                document.getElementById('grind-option').classList.remove("open");
+                document.getElementById('grind-option').nextElementSibling.style.maxHeight = null;
+            }
+            document.getElementById('grind-option').classList.add("disabled");
+        } else {
+            document.getElementById('grind-option').classList.remove("disabled");
+        }
+
+        updateOrder();
+        
+    });
+});
+
+
+function updateOrder() {
+    let coffee = type = quantity = grind = deliver = '_____';
+
+    document.getElementsByName('drink-coffee').forEach(e => { 
+        if (e.checked) {
+            coffee = e.value;
+        }
+    });
+
+    document.getElementsByName('coffee-type').forEach(e => { 
+        if (e.checked) {
+            type = e.value;
+        }
+    });
+
+    document.getElementsByName('quantity').forEach(e => { 
+        if (e.checked) {
+            quantity = e.value;
+        }
+    });
+
+    document.getElementsByName('grind').forEach(e => { 
+        if (e.checked) {
+            grind = e.value;
+        }
+    });
+
+    document.getElementsByName('deliver').forEach(e => { 
+        if (e.checked) {
+            deliver = e.value;
+        }
+    });
+
+
+    if (document.getElementById('capsule').checked){
+        html = `"I drink my coffee as <span class="selected-item">Capsules</span>, with a <span class="selected-item">${type}</span> type of bean. <span class="selected-item">${quantity}g</span> sent to me <span class="selected-item">${deliver}</span>."`;
+
+        htmlModal = html + `<p class="confirm-text">Is this correct? You can proceed to checkout or go back to plan selection if something is off. Subscription discount codes can also be redeemed at the checkout. </p><button class="btn-main checkout">Checkout - $14.00 / mo</button>`;
+    } else {
+        html = `"I drink my coffee as <span class="selected-item">${coffee}</span>, with a <span class="selected-item">${type}</span> type of bean. <span class="selected-item">${quantity}g</span> ground ala <span class="selected-item">${grind}</span>, sent to me <span class="selected-item">${deliver}</span>."`;
+
+        htmlModal = html + `<p class="confirm-text">Is this correct? You can proceed to checkout or go back to plan selection if something is off. Subscription discount codes can also be redeemed at the checkout. </p><button class="btn-main checkout">Checkout - $14.00 / mo</button>`;
+    }
+
+    document.getElementsByClassName('summary-body-text')[0].innerHTML = html;
+    document.getElementsByClassName('order-summary-body-text')[0].innerHTML = html;
+}
+
+
+
